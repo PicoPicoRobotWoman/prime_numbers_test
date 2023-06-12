038dd177485e7113cd51aaaaab54720752d133af
PicoPicoRobotWoman <nicshein.ni@gmail.com>
2023-06-12 12:18:18 +0300

    refactor: избавится от if

--- SimpleTest/src/Main.scala
+++ SimpleTest/src/Main.scala
@@ -2,13 +2,14 @@ import scala.io.StdIn
 
 object Main extends App {
 
-    val simples = List.range(2, StdIn.readInt() + 1)
-        .filter(i => (i - 1) % 6 == 0 || (i + 1) % 6 == 0)
-        .filter(i => List.range(1, (i + 1) / 2 )
-                .scanLeft(BigInt(0))((acc, value) => if( acc == 0) (i - value + 1) / value else acc * (i - value + 1) / value)
-                .map(k => k % i).sum == 0)
-        .union(List(BigInt(2), BigInt(3)))
+  val simples = List(BigInt(2), BigInt(3)).union(List.range(2, StdIn.readInt() + 1)
+    .filter(i => (i - 1) % 6 == 0 || (i + 1) % 6 == 0)
+    .filter(i => List.range(1, (i + 1) / 2)
+      .scanLeft(BigInt(1))((acc, value) => acc * (i - value + 1) / value)
+      .tail
+      .map(k => k % i).sum == 0)
+    )
 
-    println(simples)
+  println(simples.size)
 
 }
